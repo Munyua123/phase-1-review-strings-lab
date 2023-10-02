eb4a72ab60e927e0bd0938d69b0ef7811e1e7b66
Earl <earlmunyua96@gmail.com>
2023-10-02 16:42:33 +0300

    Strings Interpolation

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = "Earl Munyua"
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase() + `${currentUser}!` 
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`
